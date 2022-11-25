7e76eae3be1305ad3044a295d9bab800edfad12c
Peter <peteryurich@gmail.com>
2022-11-25 14:38:43 +0200

    init

---
+++ contacts.js
@@ -0,0 +1,55 @@
+const path = require("path");
+const fs = require("fs").promises;
+
+const contactsPath = path.resolve('./bd', 'contacts.json')
+
+async function readContacts() {
+    const contacts = await fs.readFile(contactsPath)
+        .then(data => {
+            return JSON.parse(data.toString())
+        })
+        .catch(err => console.log(err.message))
+
+    return contacts
+}
+
+async function listContacts() {
+    const contacts = await readContacts()
+
+    const contactsList = contacts.map(contact => contact.name)
+    console.table(contactsList)
+    return contactsList
+}
+
+async function getContactById(contactId) {
+    const contacts = await readContacts()
+
+    const targetContact = contacts.find(contact => contact.id === contactId)
+    console.dir(targetContact)
+    return targetContact
+}
+
+async function removeContact(contactId) {
+    const contacts = await readContacts()
+
+    const contactIndex = contacts.findIndex(contact => contact.id === contactId)
+    contacts.splice(contactIndex, 1)
+    fs.writeFile(contactsPath, JSON.stringify(contacts))
+}
+
+async function addContact(name, email, phone) {
+    const contacts = await readContacts()
+
+    const newId = String(Math.round((Math.random() * 100) * (Math.random() * 100)))
+    const newContact = { id: newId, name, email, phone }
+    contacts.push(newContact)
+    console.log(contacts)
+    fs.writeFile(contactsPath, JSON.stringify(contacts))
+}
+
+module.exports = {
+    listContacts,
+    getContactById,
+    removeContact,
+    addContact
+}
